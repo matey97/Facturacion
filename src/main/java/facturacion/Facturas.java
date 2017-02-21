efe32549da129a4d6529cb85857f0fda9fc37ea3
sergiojimenez <al341933@uji.es>
2017-02-21 12:29:00 +0100

    Con errores Creacion clase llamada y clase Facturas.

--- src/main/java/facturacion/Facturas.java
+++ src/main/java/facturacion/Facturas.java
@@ -10,4 +10,43 @@ public class Facturas {
     private int codfac;
     private Tarifa tarifa;
     private LocalDateTime fechaDeEmision;
+    private LocalDateTime Periodofact;
+    private int importe;
+
+    public Facturas(int codfac, Tarifa tarifa, LocalDateTime fechaDeEmision, LocalDateTime periodofact, int importe) {
+        this.codfac = codfac;
+        this.tarifa = tarifa;
+        this.fechaDeEmision = fechaDeEmision;
+        Periodofact = periodofact;
+        this.importe = importe;
+    }
+
+    public int getCodfac() {
+        return codfac;
+    }
+
+    public Tarifa getTarifa() {
+
+        return tarifa;
+    }
+
+    public LocalDateTime getFechaDeEmision() {
+        return fechaDeEmision;
+    }
+
+    public LocalDateTime getPeriodofact() {
+        return Periodofact;
+    }
+
+    public int getImporte() {
+        return importe;
+    }
+
+    public void setImporte(int importe) {
+        this.importe = importe;
+    }
+
+    private int calcularaImporte(int importe){
+
+    }
 }
