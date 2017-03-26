dfb8ce0f9f333e5b9fa7123ae321acefd649a326
matey97 <miguel.ms97@hotmail.es>
2017-03-26 13:16:10 +0200

    Hechos tests de Excepciones

---
+++ src/test/java/facturacion/excepciones/PrecioTarifaNoValidoTest.java
@@ -0,0 +1,48 @@
+package facturacion.excepciones;
+
+import facturacion.Excepciones.PrecioTarifaNoValido;
+import facturacion.factura.Tarifa;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by user on 26/03/17.
+ */
+public class PrecioTarifaNoValidoTest {
+
+    public static Tarifa tarifa;
+    public static boolean lanzada;
+
+    @Before
+    public void antesDeTest(){
+        tarifa=null;
+        lanzada=false;
+    }
+
+    @Test
+    public void precioTarifaNoValidoTest(){
+        System.out.println("Se debería lanzar una excepción:");
+        try{
+            tarifa=new Tarifa(-3);
+        }catch (PrecioTarifaNoValido e){
+            lanzada=true;
+            System.out.println(e);
+        }
+        if(!lanzada)
+            fail();
+    }
+
+    @Test
+    public void precioTarifaValidoTest(){
+        try{
+            tarifa=new Tarifa(7);
+        }catch (PrecioTarifaNoValido e){
+            lanzada=true;
+            System.out.println(e);
+        }
+        if(lanzada)
+            fail();
+    }
+}
