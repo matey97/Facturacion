4125fc63008ae094d34cb291fa482ba8c3867831
sergiojimenez <al341933@uji.es>
2017-04-28 17:31:53 +0200

    Desacople rama swing error en do.

---
+++ src/main/java/menu/EntradaDatosConsola.java
@@ -0,0 +1,148 @@
+package menu;
+
+
+import modelo.cliente.Cliente;
+import modelo.cliente.Direccion;
+import modelo.cliente.Empresa;
+import modelo.cliente.Particular;
+import modelo.excepciones.DuracionNoValida;
+import modelo.factura.Llamada;
+import modelo.tarifa.PromocionDomingos;
+import modelo.tarifa.PromocionTardes;
+import modelo.tarifa.Tarifa;
+import modelo.tarifa.TarifaBasica;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Scanner;
+
+
+/**
+ * Created by sergiojimenez on 28/4/17.
+ */
+public class EntradaDatosConsola {
+    public Cliente entradaDatosCliente(int n){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce nombre del cliente: ");
+        String nombre=sc.nextLine();
+        String apellidos=null;
+        if (n==1){
+            System.out.println("Introduce apellidos del cliente: ");
+            apellidos=sc.nextLine();
+        }
+
+        String NIF=entradaDatosNIF();
+
+        System.out.println("Introduce el codigo postal: ");
+        int codPostal=sc.nextInt();
+
+        System.out.println("Introduce la población: ");
+        sc.nextLine();
+        String poblacion=sc.nextLine();
+
+        System.out.println("Introduce la provincia: ");
+        String provincia=sc.nextLine();
+
+        Direccion direccion=new Direccion(codPostal,provincia,poblacion);
+
+        System.out.println("Introduce E-mail del cliente: ");
+        String email=sc.nextLine();
+
+        Tarifa tarifa = new TarifaBasica();
+
+        Cliente cliente;
+
+        if (n==1) {
+            cliente = new Particular(nombre, apellidos, NIF, direccion, email, LocalDateTime.now(), tarifa);
+        }else{
+            cliente = new Empresa(nombre,NIF,direccion,email,LocalDateTime.now(),tarifa);
+        }
+
+        return cliente;
+    }
+
+
+    public String entradaDatosNIF(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce NIF del cliente: ");
+        return sc.nextLine();
+    }
+
+    public boolean cambioTarifa(Cliente cliente){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce 1 para --> Tarifa reducida de tardes.");
+        System.out.println("Introduce 2 para --> Tarifa domingos gratis.");
+        int n=sc.nextInt();
+        if(n==1) {
+            cliente.setTarifa(new PromocionTardes(cliente.getTarifa(), 5));
+            return true;
+        }else if(n==2){
+            cliente.setTarifa(new PromocionDomingos(cliente.getTarifa()));
+            return true;
+        }
+        return false;
+    }
+
+
+    public Llamada pedirDatosLlamada() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el telefono: ");
+        int telefono = sc.nextInt();
+        System.out.println("Introduce la duración: ");
+        int duracion = sc.nextInt();
+        LocalDateTime fecha = LocalDateTime.now();
+        Llamada llamada= null;
+        try {
+            llamada= new Llamada(telefono, fecha, duracion);
+        } catch (DuracionNoValida duracionNoValida) {
+            duracionNoValida.getMessage();
+        }
+        return llamada;
+
+    }
+
+    public void imprimirLista( Collection col){
+        Iterator lista = col.iterator();
+        while(lista.hasNext()){
+            System.out.println(lista.next().toString());
+            System.out.println("");
+        }
+
+    }
+
+    public LocalDateTime pedirFechaInical(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce año en numero de la fecha de inicio: ");
+        int año = sc.nextInt();
+
+        System.out.println("Introduce mes en numero de la fecha de inicio: ");
+        int mes = sc.nextInt();
+
+        System.out.println("Introduce dia en numero de la fecha de inicio: ");
+        int dia = sc.nextInt();
+        return LocalDateTime.of(año,mes,dia,0,0);
+    }
+
+    public LocalDateTime pedirFechaFinal(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce año en numero de la fecha final: ");
+        int año2 = sc.nextInt();
+
+        System.out.println("Introduce mes en numero de la fecha final: ");
+        int mes2 = sc.nextInt();
+
+        System.out.println("Introduce dia en numero de la fecha final: ");
+        int dia2 = sc.nextInt();
+        return LocalDateTime.of(año2,mes2,dia2,0,0);
+    }
+
+
+    public int pedirCodFac(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el codigo de factura:");
+        return sc.nextInt();
+    }
+
+}
---
+++ src/main/java/menu/Main.java
@@ -0,0 +1,28 @@
+package menu;
+
+import modelo.excepciones.*;
+import modelo.Utiles;
+import modelo.cliente.Cliente;
+import modelo.cliente.Direccion;
+import modelo.factorias.*;
+import modelo.factura.Factura;
+import modelo.factura.Llamada;
+import modelo.factura.PeriodoFacturacion;
+import modelo.tarifa.Tarifa;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.util.*;
+
+/**
+ * Created by al341802 on 21/02/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+       GestionDatos datos = new GestionDatos();
+       MenuConsola MenuConsola = new MenuConsola();
+
+    }
+
+}
