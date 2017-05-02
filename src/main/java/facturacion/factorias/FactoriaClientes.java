afc809bffac76f5171c8af1a16a3a4ed6d5bd41f
sergiojimenez <al341933@uji.es>
2017-05-02 10:48:22 +0200

    creando MVC

---
+++ src/main/java/facturacion/factorias/FactoriaClientes.java
@@ -0,0 +1,25 @@
+package facturacion.factorias;
+
+import facturacion.cliente.Cliente;
+import facturacion.cliente.Direccion;
+import facturacion.cliente.Empresa;
+import facturacion.cliente.Particular;
+import facturacion.tarifa.Tarifa;
+import facturacion.tarifa.TarifaBasica;
+
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+/**
+ * Created by sergiojimenez on 4/4/17.
+ */
+public class FactoriaClientes {
+
+    public Cliente crearCliente() {
+        Cliente nuevoCliente = new Cliente() {
+
+            return nuevoCliente;
+        }
+
+    }
+}
